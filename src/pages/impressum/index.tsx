a4f0b2d6d4d6af69e30c4015c153b4592f6964b6
NoelHuibers <noel@huibers.io>
2023-08-29 20:02:18 +0200

    update

--- src/pages/impressum/index.tsx
+++ src/pages/impressum/index.tsx
@@ -32,14 +32,14 @@ const Home: NextPage = () => {
           Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und
           nach bestem Gewissen erstellt. Für die Richtigkeit, Vollständigkeit
           und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
-          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
-          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
-          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
-          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
-          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
-          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
-          Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
-          hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+          Als Diensteanbieter sind wir gemäß §7 Abs.1 TMG für eigene Inhalte auf
+          diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
+          bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+          übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+          nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+          hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung
+          von Informationen nach den allgemeinen Gesetzen bleiben hiervon
+          unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
           Zeitpunkt der Kenntniserlangung einer konkreten Rechtsverletzung
           möglich. Bei Bekanntwerden von den o.g. Rechtsverletzungen werden wir
           diese Inhalte unverzüglich entfernen.{" "}
