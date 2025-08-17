d9e1d86edf3c4a1995ca398caba80167b3ee132f
NoelHuibers <noel@huibers.io>
2025-08-18 01:30:52 +0200

    Speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
 import "~/styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 
 export const metadata: Metadata = {
-	title: "Rhenania Stuttgart",
-	description: "Corps Rhenania Stuttgart",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: "Rhenania Stuttgart",
+  description: "Corps Rhenania Stuttgart",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 const geist = Geist({
-	subsets: ["latin"],
-	variable: "--font-geist-sans",
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<html lang="en" className={`${geist.variable}`}>
-			<body>{children}</body>
-			<Analytics />
-		</html>
-	);
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>{children}</body>
+      <Analytics />
+      <SpeedInsights />
+    </html>
+  );
 }
